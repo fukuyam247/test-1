692128512c52631be5f0c0b5aeb4e8e1fa30cfc3
fukuyama247 <fukuyama247@gmai.com>
2024-10-01 14:05:10 +0900

    mivingGoal追加
    vector3を使用してサンプルコード通りに記述。
    x軸上に平行移動するため、これをz軸上で移動するように変更したい。

---
+++ Assets/movinggoal.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movinggoal : MonoBehaviour
+{
+    private Vector3 initialPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Sin(Time.time) * 2.0f + initialPosition.x, initialPosition.y, initialPosition.z);
+    }
+
+}
+
