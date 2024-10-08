fc89c789fc2f7c5aef67eb2638c42b8d0e0ff4f6
fukuyama247 <fukuyama247@gmai.com>
2024-10-08 13:43:24 +0900

    2024/9/29 #2

--- Assets/Goalmethod.cs
+++ Assets/Goalmethod.cs
@@ -16,6 +16,7 @@ public class Goalmethod : MonoBehaviour
         
     }
 
+    //このスクリプトを与えられたオブジェクトがtag”プレイヤー”に当たったら「当たってきたオブジェクトを」消す
     private void OnCollisionEnter(Collision other)
     {
         if(other.gameObject.tag == "Player")
